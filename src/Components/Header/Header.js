1348570f316a77a7681011e60c829b2d9acebe79
kimthunbunly <kimthunbunly.rupp@gmail.com>
2018-06-22 09:19:38 +0700

    Update all code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,23 +9,24 @@ export default class Header extends React.Component{
   render(){
     return(
         <nav className="navbar navbar-expand-lg navbar-dark change-color navbar-height">
+          <div className="container">
             <img src={logo} width="130" height="auto" className="d-inline-block align-top img-fluid" alt="logo"/>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <div className="mr-auto"/>
-              <ul className="nav ">
-                <li className="nav-item ">
-                  <Link to='/Homepage' className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to='/ClientsServices'>Service</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to='/Contacts'>Contact</Link>
-                </li>
-              </ul>
+                <div className="mr-auto"/>
+                  <ul className="nav ">
+                    <li className="nav-item ">
+                      <Link to='/Homepage' className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to='/ClientsServices'>Service</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to='/Contacts'>Contact</Link>
+                    </li>
+                  </ul>
                 <div className="nav-item dropdown show">
                   <a className="btn dropdown-toggle" href="" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <img src={profile} alt="profile" width="25"/>
@@ -40,6 +41,7 @@ export default class Header extends React.Component{
                   </div>
                 </div>
               </div>
+            </div>
         </nav>
     );
   }
